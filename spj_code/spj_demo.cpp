80087be54ca3db5eccf1ff2273dc7fd644523b06
Kalzncc <44296812+Kalzncc@users.noreply.github.com>
2021-07-10 08:21:32 +0800

    Update spj demo code displayed in readme.md

--- spj_code/spj_demo.cpp
+++ spj_code/spj_demo.cpp
@@ -1,12 +1,18 @@
 #include "spjlib.h"
 int main(int argc, char * argv[]) {
-
     START_JUDGE();
 
 
-    // 写代码
+//------------SPJ--CODE--------------------
+    int a; fscanf(input, "%d", &a);
+    int b; fscanf(output, "%d", &b);
+    int c; fscanf(stdoutput, "%d", &c);
+
+    if (/*some conditions*/ )
+        EXIT_JUDGE(ACCEPTED); 
+    else EXIT_JUDGE(WRONG_ANSWER); 
+//------------SPJ--CODE--------------------
 
 
     EXIT_JUDGE(ACCEPTED); 
-    
 }
