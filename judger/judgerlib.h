a51dfec926e72395998aed437642d17cbec0748b
Kalzncc <1286298430@qq.com>
2021-07-17 09:00:23 +0800

    rename dir name

---
+++ judger/judgerlib.h
@@ -0,0 +1,10 @@
+
+#ifndef JUDGERLIB_H
+#define JUDGERLIB_H
+#include "judger_config.h"
+
+
+RunConfig* judge(const JudgerConfig * judgerConfig, const JudgeConfig *judgeConfig);
+
+
+#endif
---
+++ judger/loglib.h
@@ -0,0 +1,14 @@
+#ifndef LOGLIB_H
+#define LOGLIB_H
+
+#define MAX_LOG_LENGTH 1024
+#include "judger_config.h"
+
+
+void logDebugInfoWithMessage(const char *path, const char *message);
+void logSystemErrorWithResult(const char * path, const RunConfig* result);
+void logSystemErrorWithMessage(const char * path, JudgeError error, const char * message);
+void logSystemErrorWithTaskID(const char * path, int taskID, JudgeError error, const char * message);
+void createSystemError(RunConfig * result, JudgeError error, const char * message, const char * logPath);
+
+#endif
